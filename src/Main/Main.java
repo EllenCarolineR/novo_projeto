23e95956fa69dcdbbbf3e37b33153d0099b690a8
Radames <rjhalmeman@gmail.com>
2018-03-12 09:39:11 -0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,32 +10,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //versao 0 notebook
-        //mudei pelo site
-        
-        //mudei pelo notebook e vou enviar
-        
-        //mudei pelo site, agora vou tentar puxar para o notebook pelo netbeans
-        //   reverter quer dizer.... buscar no site e considerar que esta é a versão correta....
-        // chegou no notebook...
-        
-        //mais uma modificação 10h27
-        
-        
-        //agora vai... eu modifico aqui e aparece um pull request no site
-        
-        //estou modificando com o desktop
-        
-        
-        //modifiquei com notebook
-        
-        //vai que é sua notebook
-        
-        //fiz com notebook, commit e enviei...
-        
-        //modifiquei com desktop
-        
-        //dupla modificacao notebook
+     // Testes no github
     }
     
 }
